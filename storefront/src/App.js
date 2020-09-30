81fba66fb4bb2335920f4812495682090093f668
Jesse Dillman <jesse@jdillman.com>
2020-09-30 01:02:56 -0400

    fetch api

--- storefront/src/App.js
+++ storefront/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './App.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const [products, setProducts] = useState( [] );
+	const [readError, setReadError] = useState( [] );
+
+	useEffect( () => {
+		setReadError( null );
+		try {
+			fetch('http://localhost:3030/products?$limit=10')
+			.then((resp) => resp.json())
+			.then((data) => {
+				let items = [];
+				data.data.forEach( ( item ) => {
+					items.push( item );
+				} );
+				setProducts( items );
+			})
+			.catch((err) => console.error("Error fetching products: " + err));
+		} catch ( error ) {
+			setReadError( error.message );
+		}
+	} );
+
+	return (
+		<div className="App">
+			{readError}
+			{ products.map( product => {
+				return (
+					<div key={product.id}>
+						<h3>{product.name}</h3>
+						<p>{product.price}</p>
+					</div>
+				)
+			} ) }
+		</div>
+	);
 }
 
 export default App;
